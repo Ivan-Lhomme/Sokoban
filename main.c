09f4346935157efcd8be4e40b3c135814f6a4d5c
jsp <jsp@rien.com>
2025-10-26 23:14:07 +0100

    Add comment to main.c

--- main.c
+++ main.c
@@ -11,30 +11,31 @@ int main()
 	system("clear");
 	printf("Bienvenue dans mon Sokoban fait maison !\n");
 
-	int win;
-	int verif;
-	int nbr_entities;
+	int win;									// use to check if the player win
+	int check;									// use to check if a plyer enter only a character
+	int nbr_entities;							// stores the amount of boxes and targets
 
-	int ** coordinates;
+	int ** coordinates;							// stores all coodinates of all entities (player, boxes, targets)
 
-	char direction;
-	char restart;
+	char direction;								// store the direction of player
+	char restart;								// use to ask to the user if he want to restart
 
-	char ** level;
+	char ** level;								// stores the whole level
 
 	entity * player;
 	entity ** boxes = malloc(sizeof(*boxes));
 	entity ** targets = malloc(sizeof(*targets));
 
-	int count_t[4];
+	int count_t[4];								// stores the amount of targets near walls {top, bottom, left, right}
 
+	// loop of the whole game
 	do {
 		srand(time(NULL));
 
-		win = -1;
-		level = import_level(1);
-		nbr_entities = (rand() % 5) + 1;
-		for (int i = 0; i < 4; i++) {
+		win = -1;								// -1 meens the user don t win and don t loose
+		level = import_level(1); 				// import the level from a file
+		nbr_entities = (rand() % 6) + 1;		// min : 1 | max : 5
+		for (int i = 0; i < 4; i++) {			// reset count_t every restart
 			count_t[i] = 0;
 		}
 
@@ -48,18 +49,19 @@ int main()
 		boxes = create_entities('X', boxes, nbr_entities, coordinates, 1);
 		targets = create_entities('.', targets, nbr_entities, coordinates, nbr_entities + 1);
 
-		count_entity_wall(targets, count_t);
+		count_entity_wall(targets, count_t);	// count number of targets near walls
 
+		// loop of the game
 		while (win == -1) {
 			draw_entity(level, player, boxes, targets);
 			display_level(level);
 
 			do {
 				printf("Entrer une direction (zqsd) :\n");
-				verif = scanf("%c", &direction);
+				check = scanf("%c", &direction);
 
-				while (getchar() != '\n') {}
-			} while (verif == 0);
+				while (getchar() != '\n') {}	// delete all input in stdint
+			} while (check == 0);
 
 			move(level, player, direction, boxes);
 
@@ -74,7 +76,7 @@ int main()
 
 		draw_entity(level, player, boxes, targets);
 		display_level(level);
-		save_level(level);
+		save_level(level);						// save the end of game in a file 
 
 		if (win == 1) {
 			printf("Bien joue, vous avez gagne !\n");
@@ -85,12 +87,13 @@ int main()
 		}
 
 		printf("Si tu veux recommence entre 'y' sinon n'importe quel lettre.\n");
-		verif = scanf("%c", &restart);
+		check = scanf("%c", &restart);
 
 		while (getchar() != '\n') {}
 		system("clear");
 	} while (restart == 'y');
 
+	// free all dynamically allocated variables
 	int i = 0;
 	while (boxes[i] != NULL) {
 		free(boxes[i]);
